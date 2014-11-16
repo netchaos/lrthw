934175397219e4dc81df603e1fdba5259d50c4ec
Vimal Kurup <contact.vimal@yahoo.com>
2014-11-16 01:57:34 -0500

    more excercise

---
+++ ex12.rb
@@ -0,0 +1,4 @@
+#Find percentage
+puts "Enter the amount"
+amount = gets.chomp.to_f
+puts amount * 0.10
---
+++ ex13.rb
@@ -0,0 +1,7 @@
+#Passing arguments in command line
+first, second, third = ARGV
+
+puts "Yor first variable is: #{first}"
+puts "Yor second variable is: #{second}"
+puts "Yor third variable is: #{third}"
+
