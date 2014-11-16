cfb7ffa6231dcea10b7162ab8f3fb03d2afbee35
Vimal Kurup <contact.vimal@yahoo.com>
2014-11-16 00:54:17 -0500

    add exercise files

---
+++ ex9.rb
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+There's something going on here.
+With the three doble-quotes.
+We'll be avle to type as much as we like.
+Even 4 line if we want, or 5, or 6.
+"""
